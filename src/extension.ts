8dab3f60d39f5bf2ade01f29388805b16ae833b2
Illyrius <FitimQ@live.nl>
2023-11-21 17:01:29 +0100

    Add StenAPS setup command

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,33 @@
 import * as vscode from "vscode";
+import * as path from "path";
 
-export function activate(context: vscode.ExtensionContext) {}
+const extensionName = "StenAPS";
+const extensionASCII = "";
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log("Congratulations, your extension is now active!");
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(`"${extensionName}.Setup"`, async () => {
+      const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+
+      if (workspaceFolder) {
+        const setupScriptPath = path.join(
+          workspaceFolder.uri.fsPath,
+          "setup.ps1"
+        );
+        const terminal = vscode.window.createTerminal();
+        terminal.show();
+        terminal.sendText(`echo "${extensionASCII}"`);
+        terminal.sendText(
+          `powershell -ExecutionPolicy Bypass -File "${setupScriptPath}"`
+        );
+        terminal.show();
+      } else {
+        vscode.window.showErrorMessage(
+          "No workspace folder found. Please open a workspace folder and try again."
+        );
+      }
+    })
+  );
+}
