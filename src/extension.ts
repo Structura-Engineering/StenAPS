f8cfaacd91049daac948a66c3a9ee143c06cecba
Illyrius <FitimQ@live.nl>
2023-11-21 15:51:36 +0100

    Add child_process module and icon.png

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,3 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "StenAPS" is now active!');
-
-  const helloWorldCommand = vscode.commands.registerCommand(
-    "StenAPS.helloWorld",
-    () => {
-      vscode.window.showInformationMessage("Hello World from StenAPS!");
-    }
-  );
-
-  context.subscriptions.push(helloWorldCommand);
-}
-
-export function deactivate() {}
+export function activate(context: vscode.ExtensionContext) {}
