bb71c02473e9dfcc179f1a19e7c47ccb6401d32e
Illyrius <28700752+illyrius666@users.noreply.github.com>
2023-11-22 18:55:55 +0100

    Refactor createWebview function to use ExtensionContext

--- src/ts/webview.ts
+++ src/ts/webview.ts
@@ -1,43 +1,32 @@
-import { window, ViewColumn, workspace, Uri } from "vscode";
+import { window, ViewColumn, Uri, ExtensionContext } from "vscode";
 import { join } from "path";
 import { readFileSync } from "fs";
 
 /**
  * Creates a webview panel and loads the html content.
  */
-export function createWebview() {
-  if (workspace.workspaceFolders) {
-    const panel = window.createWebviewPanel(
-      "webview",
-      "StenAPS",
-      ViewColumn.One,
-      {
-        enableScripts: true,
-        localResourceRoots: [
-          Uri.file(join(workspace.workspaceFolders[0].uri.fsPath, "src")),
-        ],
-      }
-    );
+export function createWebview(context: ExtensionContext) {
+  const panel = window.createWebviewPanel(
+    "webview",
+    "StenAPS",
+    ViewColumn.One,
+    {
+      enableScripts: true,
+      localResourceRoots: [Uri.file(join(context.extensionPath, "src"))],
+    }
+  );
 
-    const htmlPath = Uri.file(
-      join(
-        workspace.workspaceFolders[0].uri.fsPath,
-        "src",
-        "html",
-        "index.html"
-      )
-    );
-    let htmlContent = readFileSync(htmlPath.fsPath, "utf8");
+  const htmlPath = Uri.file(
+    join(context.extensionPath, "src", "html", "index.html")
+  );
+  let htmlContent = readFileSync(htmlPath.fsPath, "utf8");
 
-    const srcPath = panel.webview.asWebviewUri(
-      Uri.file(join(workspace.workspaceFolders[0].uri.fsPath, "src"))
-    );
+  const srcPath = panel.webview.asWebviewUri(
+    Uri.file(join(context.extensionPath, "src"))
+  );
 
-    htmlContent = htmlContent.replace(/src="\.\.\//g, `src="${srcPath}/`);
-    htmlContent = htmlContent.replace(/href="\.\.\//g, `href="${srcPath}/`);
+  htmlContent = htmlContent.replace(/src="\.\.\//g, `src="${srcPath}/`);
+  htmlContent = htmlContent.replace(/href="\.\.\//g, `href="${srcPath}/`);
 
-    panel.webview.html = htmlContent;
-  } else {
-    console.error("No workspace folders found");
-  }
+  panel.webview.html = htmlContent;
 }
