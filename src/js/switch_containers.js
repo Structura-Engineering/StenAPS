1217481ab1e65b487eb7d34cf7789ec21602451a
Illyrius <28700752+illyrius666@users.noreply.github.com>
2023-11-22 21:43:43 +0100

    Update CSS and HTML files

---
+++ src/js/switch_containers.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+  var mainBody = document.getElementById("main");
+  var wizardBody = document.getElementById("wizard");
+
+  var docsItem = document.getElementById("docs");
+  var cppItem = document.getElementById("cpp");
+  var pyItem = document.getElementById("py");
+
+  var toolItems = [docsItem, cppItem, pyItem];
+  toolItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      console.log("clicked");
+      mainBody.style.display = "none";
+      wizardBody.style.display = "block";
+    });
+  });
+
+  var returnButton = document.getElementById("return-button");
+
+  returnButton.addEventListener("click", function () {
+    console.log("clicked");
+    wizardBody.style.display = "none";
+    mainBody.style.display = "block";
+  });
+};
