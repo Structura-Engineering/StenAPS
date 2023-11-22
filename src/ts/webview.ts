25d8c3d41f3a91b59cba3a4e25ac37d66af36352
Illyrius <28700752+illyrius666@users.noreply.github.com>
2023-11-22 18:38:47 +0100

    Refactor webview creation and path replacement

--- src/ts/webview.ts
+++ src/ts/webview.ts
@@ -2,43 +2,44 @@ import { window, ViewColumn, workspace, Uri } from "vscode";
 import { join } from "path";
 import { readFileSync } from "fs";
 
+const SRC_REGEX = /src="\.\.\//g;
+const HREF_REGEX = /href="\.\.\//g;
+
+/**
+ * Replaces the given regex in the content with the new path.
+ */
+function replacePath(content: string, regex: RegExp, newPath: string) {
+  return content.replace(regex, `${newPath}/`);
+}
+
 /**
  * Creates a webview panel and loads the html content.
  */
 export function createWebview() {
-  // Check if workspaceFolders is defined
-  if (workspace.workspaceFolders) {
-    const panel = window.createWebviewPanel(
-      "webview",
-      "StenAPS",
-      ViewColumn.One,
-      {
-        enableScripts: true,
-        localResourceRoots: [
-          Uri.file(join(workspace.workspaceFolders[0].uri.fsPath, "src")),
-        ],
-      }
-    );
-
-    const htmlPath = Uri.file(
-      join(
-        workspace.workspaceFolders[0].uri.fsPath,
-        "src",
-        "html",
-        "index.html"
-      )
-    );
-    let htmlContent = readFileSync(htmlPath.fsPath, "utf8");
-
-    const srcPath = panel.webview.asWebviewUri(
-      Uri.file(join(workspace.workspaceFolders[0].uri.fsPath, "src"))
-    );
-
-    htmlContent = htmlContent.replace(/src="\.\.\//g, `src="${srcPath}/`);
-    htmlContent = htmlContent.replace(/href="\.\.\//g, `href="${srcPath}/`);
-
-    panel.webview.html = htmlContent;
-  } else {
-    console.error("No workspace folders found");
+  if (!workspace.workspaceFolders) {
+    throw new Error("No workspace folders found");
   }
+
+  const workspacePath = workspace.workspaceFolders[0].uri.fsPath;
+  const srcPath = Uri.file(join(workspacePath, "src"));
+
+  const panel = window.createWebviewPanel(
+    "webview",
+    "StenAPS",
+    ViewColumn.One,
+    {
+      enableScripts: true,
+      localResourceRoots: [srcPath],
+    }
+  );
+
+  const htmlPath = Uri.file(join(workspacePath, "src", "html", "index.html"));
+  let htmlContent = readFileSync(htmlPath.fsPath, "utf8");
+
+  const webviewSrcPath = panel.webview.asWebviewUri(srcPath).toString();
+
+  htmlContent = replacePath(htmlContent, SRC_REGEX, webviewSrcPath);
+  htmlContent = replacePath(htmlContent, HREF_REGEX, webviewSrcPath);
+
+  panel.webview.html = htmlContent;
 }
