fa53d6af81406723bfc0d67968ef6a3e82389f10
Illyrius <28700752+illyrius666@users.noreply.github.com>
2023-11-22 18:35:19 +0100

    Refactor webview creation and resource paths

--- src/ts/webview.ts
+++ src/ts/webview.ts
@@ -6,35 +6,39 @@ import { readFileSync } from "fs";
  * Creates a webview panel and loads the html content.
  */
 export function createWebview() {
-  const panel = window.createWebviewPanel(
-    "webview",
-    "StenAPS",
-    ViewColumn.One,
-    {
-      enableScripts: true,
-      localResourceRoots: [
-        Uri.file(join(workspace.workspaceFolders![0].uri.fsPath, "src")),
-      ],
-    }
-  );
+  // Check if workspaceFolders is defined
+  if (workspace.workspaceFolders) {
+    const panel = window.createWebviewPanel(
+      "webview",
+      "StenAPS",
+      ViewColumn.One,
+      {
+        enableScripts: true,
+        localResourceRoots: [
+          Uri.file(join(workspace.workspaceFolders[0].uri.fsPath, "src")),
+        ],
+      }
+    );
 
-  const htmlPath = Uri.file(
-    join(workspace.workspaceFolders![0].uri.fsPath, "src", "html", "index.html")
-  );
-  let htmlContent = readFileSync(htmlPath.fsPath, "utf8");
+    const htmlPath = Uri.file(
+      join(
+        workspace.workspaceFolders[0].uri.fsPath,
+        "src",
+        "html",
+        "index.html"
+      )
+    );
+    let htmlContent = readFileSync(htmlPath.fsPath, "utf8");
 
-  const srcPath = panel.webview.asWebviewUri(
-    Uri.file(join(workspace.workspaceFolders![0].uri.fsPath, "src"))
-  );
+    const srcPath = panel.webview.asWebviewUri(
+      Uri.file(join(workspace.workspaceFolders[0].uri.fsPath, "src"))
+    );
 
-  htmlContent = htmlContent.replace(
-    new RegExp('src="../', "g"),
-    `src="${srcPath}/`
-  );
-  htmlContent = htmlContent.replace(
-    new RegExp('href="../', "g"),
-    `href="${srcPath}/`
-  );
+    htmlContent = htmlContent.replace(/src="\.\.\//g, `src="${srcPath}/`);
+    htmlContent = htmlContent.replace(/href="\.\.\//g, `href="${srcPath}/`);
 
-  panel.webview.html = htmlContent;
+    panel.webview.html = htmlContent;
+  } else {
+    console.error("No workspace folders found");
+  }
 }
