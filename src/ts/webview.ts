292ddc8a3d77db43a2866ca0aa11089cbaa6fb5f
Illyrius <28700752+illyrius666@users.noreply.github.com>
2023-11-22 18:29:10 +0100

    Update CSS and webview.ts files

--- src/ts/webview.ts
+++ src/ts/webview.ts
@@ -1,5 +1,6 @@
-import { window, ViewColumn, workspace } from "vscode";
+import { window, ViewColumn, workspace, Uri } from "vscode";
 import { join } from "path";
+import { readFileSync } from "fs";
 
 /**
  * Creates a webview panel and loads the html content.
@@ -9,18 +10,31 @@ export function createWebview() {
     "webview",
     "StenAPS",
     ViewColumn.One,
-    {}
+    {
+      enableScripts: true,
+      localResourceRoots: [
+        Uri.file(join(workspace.workspaceFolders![0].uri.fsPath, "src")),
+      ],
+    }
   );
 
-  if (workspace.workspaceFolders) {
-    const htmlPath = join(
-      workspace.workspaceFolders[0].uri.fsPath,
-      "./src/html/index.html"
-    );
-    panel.webview.html = htmlPath;
-  } else {
-    window.showErrorMessage(
-      "No workspace opened. Please open a workspace first."
-    );
-  }
+  const htmlPath = Uri.file(
+    join(workspace.workspaceFolders![0].uri.fsPath, "src", "html", "index.html")
+  );
+  let htmlContent = readFileSync(htmlPath.fsPath, "utf8");
+
+  const srcPath = panel.webview.asWebviewUri(
+    Uri.file(join(workspace.workspaceFolders![0].uri.fsPath, "src"))
+  );
+
+  htmlContent = htmlContent.replace(
+    new RegExp('src="../', "g"),
+    `src="${srcPath}/`
+  );
+  htmlContent = htmlContent.replace(
+    new RegExp('href="../', "g"),
+    `href="${srcPath}/`
+  );
+
+  panel.webview.html = htmlContent;
 }
