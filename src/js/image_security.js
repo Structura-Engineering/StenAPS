74478d8578ff87547aac245d04894912d6b03169
Illyrius <FitimQ@live.nl>
2023-11-23 15:11:40 +0100

    Refactor CSS and JavaScript code

--- src/js/image_security.js
+++ src/js/image_security.js
@@ -1,65 +1,52 @@
 import { md5 } from "./md5.js";
 
-/**
- * Represents an ImageSecurity instance that obfuscates image URLs and prevents certain actions on images.
- */
 class ImageSecurity {
-  /**
-   * Create an ImageSecurity instance.
-   * Initializes instance properties and sets up event listeners.
-   * Automatically obfuscates image URLs on initialization.
-   */
-  constructor(imageElement) {
-    this.image = imageElement;
+  constructor() {
     this.images = document.querySelectorAll("img[data-obfuscate]");
-
     this.addEventListeners();
     this.obfuscateImageURLs();
   }
 
-  /**
-   * Add event listeners to images to prevent right-click, dragging, and specific key combinations.
-   */
   addEventListeners() {
-    // Prevent right-click and dragging on images
-    const disableEvent = (event) => event.preventDefault();
+    document.addEventListener("contextmenu", (event) => {
+      if (event.target.matches("img[data-obfuscate]")) {
+        event.preventDefault();
+      }
+    });
 
-    this.images.forEach((image) => {
-      image.addEventListener("contextmenu", disableEvent);
-      image.addEventListener("dragstart", disableEvent);
+    document.addEventListener("dragstart", (event) => {
+      if (event.target.matches("img[data-obfuscate]")) {
+        event.preventDefault();
+      }
+    });
 
-      image.addEventListener("keydown", (event) => {
-        if (event.ctrlKey && (event.key === "c" || event.key === "i")) {
-          event.preventDefault();
-        }
-      });
+    document.addEventListener("keydown", (event) => {
+      if (
+        event.ctrlKey &&
+        (event.key === "c" || event.key === "i") &&
+        event.target.matches("img[data-obfuscate]")
+      ) {
+        event.preventDefault();
+      }
     });
   }
 
-  /**
-   * Generate a unique filename for an image based on its original filename.
-   * @static
-   */
   static generateUniqueFilename(originalFilename) {
     const hash = md5(originalFilename);
-    const fileExtension = originalFilename.split(".").pop();
+    const fileExtension = originalFilename.substring(
+      originalFilename.lastIndexOf(".") + 1
+    );
     const uniqueFilename = `${hash}.${fileExtension}`;
     return uniqueFilename;
   }
 
-  /**
-   * Obfuscate image URLs by replacing them with unique filenames.
-   */
   obfuscateImageURLs() {
     this.images.forEach((image) => {
       const originalSrc = image.getAttribute("src");
-      const uniqueFilename = this.generateUniqueFilename(originalSrc);
+      const uniqueFilename = ImageSecurity.generateUniqueFilename(originalSrc);
       image.setAttribute("src", uniqueFilename);
     });
   }
 }
 
-// Wait for the DOM to be fully loaded before creating a new instance of ImageSecurity
-document.addEventListener("DOMContentLoaded", function () {
-  const _ = new ImageSecurity();
-});
+document.addEventListener("DOMContentLoaded", () => new ImageSecurity());
--- src/js/switch_containers.js
+++ src/js/switch_containers.js
@@ -1,25 +1,28 @@
-window.onload = function () {
-  var mainBody = document.getElementById("primary");
-  var wizardBody = document.getElementById("secondary");
+class ContainerSwitcher {
+  constructor() {
+    this.mainBody = document.querySelector(".container#primary");
+    this.wizardBody = document.querySelector(".container#secondary");
 
-  var docsItem = document.getElementById("docs");
-  var cppItem = document.getElementById("cpp");
-  var pyItem = document.getElementById("py");
+    this.cppItem = document.getElementById("cpp");
+    this.pyItem = document.getElementById("py");
+    this.nodejsItem = document.getElementById("nodejs");
 
-  var toolItems = [docsItem, cppItem, pyItem];
-  toolItems.forEach(function (item) {
-    item.addEventListener("click", function () {
-      console.log("clicked");
-      mainBody.style.display = "none";
-      wizardBody.style.display = "block";
+    this.toolItems = [this.nodejsItem, this.cppItem, this.pyItem];
+    this.toolItems.forEach((item) => {
+      item.addEventListener("click", () => {
+        console.log("clicked");
+        this.mainBody.style.display = "none";
+        this.wizardBody.style.display = "block";
+      });
     });
-  });
 
-  var returnButton = document.getElementById("return-button");
+    this.returnButton = document.getElementById("return-button");
+    this.returnButton.addEventListener("click", () => {
+      console.log("clicked");
+      this.wizardBody.style.display = "none";
+      this.mainBody.style.display = "block";
+    });
+  }
+}
 
-  returnButton.addEventListener("click", function () {
-    console.log("clicked");
-    wizardBody.style.display = "none";
-    mainBody.style.display = "block";
-  });
-};
+document.addEventListener("DOMContentLoaded", () => new ContainerSwitcher());
